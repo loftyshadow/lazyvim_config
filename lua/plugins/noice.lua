4ef4969bc80508cc05935d671e3e975132ed2e95
niemingzhi <1094290505@qq.com>
2024-02-06 00:58:33 +0800

    feat: change plugins opt

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    cmdline = {
+      enabled = true,
+      view = "cmdline",
+    },
+  },
+}
--- lua/plugins/persisted.lua
+++ lua/plugins/persisted.lua
@@ -2,9 +2,9 @@ return {
   {
     "olimorris/persisted.nvim",
     keys = {
-      { "<leader>Sf", "<cmd>Telescop persisted<cr>" },
-      { "<leader>Ss", "<cmd>SessionSave<cr>" },
-      { "<leader>Sd", "<cmd>SessionDelete<cr>" },
+      { "<leader>Sf", "<cmd>Telescop persisted<cr>", desc = "find all session" },
+      { "<leader>Ss", "<cmd>SessionSave<cr>", desc = "save current session" },
+      { "<leader>Sd", "<cmd>SessionDelete<cr>", desc = "delete current session" },
     },
     opts = {
       -- add your options that should be passed to the setup() function here
