d73e81b08105b30b5401db494e63efc939b32127
niemingzhi <1094290505@qq.com>
2024-02-06 10:39:24 +0800

    feat: add toggle term

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,3 +1,4 @@
 return {
   { "folke/persistence.nvim", enable = false },
+  { "folke/tokyonight.nvim", enable = false },
 }
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    open_mapping = [[<leader>tt]],
+    start_in_start = true,
+    direction = "float",
+    float_opts = {
+      border = "curved",
+      width = 130,
+      height = 30,
+    },
+  },
+}
