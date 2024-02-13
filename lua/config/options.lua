bb0d91885acf300179a2111c708dcfe0894611f7
niemingzhi <1094290505@qq.com>
2024-02-13 19:39:07 +0800

    feat: add tab options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 vim.opt.modified = true
 vim.opt.clipboard:append("unnamedplus")
+
+-- 缩进
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
